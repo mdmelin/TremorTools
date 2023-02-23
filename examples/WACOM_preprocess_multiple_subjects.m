d0f0bb7bc25448cc38971ba27b3f6f30896f9474
Max Melin <mmelin@g.ucla.edu>
2023-02-22 19:14:10 -0800

    addpath

--- examples/WACOM_preprocess_multiple_subjects.m
+++ examples/WACOM_preprocess_multiple_subjects.m
@@ -1,4 +1,5 @@
 clc;clear all;close all;
+addpath(fileparts(cd));
 %% preprocess WACOM data for multiple subjects
 subject = {'FUS003','FUS0016','FUS0024','FUS0041'}; %cell of subjects, can contain multiple subjects
 %subject = {'FUS0024','FUS0041'}; %cell of subjects, can contain multiple subjects
@@ -7,8 +8,6 @@ datadir = 'C:\Users\mmelin\Desktop\tremor\data\'; %path to data server, this sho
 savedir = 'C:\Users\mmelin\Desktop\tremor\WACOMpreprocessed\'; %where to put the preprocessed data
 
 for x = 1:length(subject)
-
     WACOM_reformat_preprocess(datadir,savedir,subject{x},'pre');
     WACOM_reformat_preprocess(datadir,savedir,subject{x},'post');
-
 end
