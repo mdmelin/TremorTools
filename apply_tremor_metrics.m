ceecad484e0b56726f9dc8810838ddb97e30d04f
Max Melin <mmelin@g.ucla.edu>
2023-09-05 12:38:33 -0700

    tremor detection

---
+++ apply_tremor_metrics.m
@@ -0,0 +1,16 @@
+function has_tremor = apply_tremor_metrics(peak_freqs, half_bandwidths)
+
+MIN_TREMOR_FREQ = 3.7;
+MAX_TREMOR_FREQ = 10;
+MAX_HALF_BANDWIDTH = 2;
+
+assert(length(peak_freqs) == length(half_bandwidths))
+
+for i=1:length(peak_freqs)
+    if peak_freqs(i) > MIN_TREMOR_FREQ && peak_freqs(i) < MAX_TREMOR_FREQ && half_bandwidths(i) < MAX_HALF_BANDWIDTH
+        has_tremor(i) = 1;
+    else
+        has_tremor(i) = 0;
+end
+
+end
